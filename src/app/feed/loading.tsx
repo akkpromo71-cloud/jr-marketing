ad2bb8c5eb051c43e99af1f3e90e70cedc4e3c14
akkpromo71-cloud <akkpromo71@gmail.com>
2026-09-08 01:41:53 +0500

    Update loading.tsx

--- src/app/feed/loading.tsx
+++ src/app/feed/loading.tsx
@@ -5,18 +5,18 @@
 export default function FeedLoading() {
   return (
     <main className="mx-auto max-w-4xl px-6 py-12">
-      <div className="skeleton h-9 w-56 rounded-lg" />
-      <div className="skeleton mt-3 h-4 w-80 rounded-lg" />
+      <div className="skeleton h-9 w-56 rounded-none" />
+      <div className="skeleton mt-3 h-4 w-80 rounded-none" />
 
       <div className="mt-8 flex flex-col gap-4">
         {[0, 1, 2].map((i) => (
-          <div key={i} className="rounded-2xl border border-border bg-surface p-6">
+          <div key={i} className="rounded-none border border-border bg-surface p-6">
             <div className="flex items-start justify-between gap-4">
               <div className="flex-1">
-                <div className="skeleton h-6 w-2/3 rounded-lg" />
-                <div className="skeleton mt-2 h-3 w-1/3 rounded-lg" />
-                <div className="skeleton mt-3 h-4 w-full rounded-lg" />
-                <div className="skeleton mt-2 h-4 w-5/6 rounded-lg" />
+                <div className="skeleton h-6 w-2/3 rounded-none" />
+                <div className="skeleton mt-2 h-3 w-1/3 rounded-none" />
+                <div className="skeleton mt-3 h-4 w-full rounded-none" />
+                <div className="skeleton mt-2 h-4 w-5/6 rounded-none" />
               </div>
               <div className="skeleton h-6 w-16 shrink-0 rounded-full" />
             </div>
